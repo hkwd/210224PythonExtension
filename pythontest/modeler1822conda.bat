4cc84dccebee5387eef9be5769fa5659704fbc1f
HAJIME KAWADA <hkwd@jp.ibm.com>
2021-03-23 17:29:08 +0900

    add a new project

---
+++ pythontest/modeler1822conda.bat
@@ -0,0 +1,3 @@
+set Path=C:\Users\dsuser01\.conda\envs\modeler;C:\Users\dsuser01\.conda\envs\modeler\Library\mingw-w64\bin;C:\Users\dsuser01\.conda\envs\modeler\Library\usr\bin;C:\Users\dsuser01\.conda\envs\modeler\Library\bin;C:\Users\dsuser01\.conda\envs\modeler\Scripts;C:\Users\dsuser01\.conda\envs\modeler\bin;C:\ProgramData\Miniconda3\condabin;%PATH%
+rem echo %Path%
+start "" "C:\Program Files\IBM\SPSS\Modeler\18.2.2\bin\modelerclient.exe"
